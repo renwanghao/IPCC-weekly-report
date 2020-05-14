a0f7cf7f339cdd6dc365d41d5724721117f210a6
EN1ACOC289 <62944908+EN1ACOC289@users.noreply.github.com>
2020-05-14 09:38:20 +0800

    Add files via upload

---
+++ ZhiliChen/code1.3(experiment)/experiment.py
@@ -0,0 +1,88 @@
+from Quantum_Circuit import AdversarialQCircuit as QC
+import numpy as np
+from matplotlib import pyplot as plt
+import copy
+import pickle
+from iRprop import iteration
+
+
+def save_figure(noise_type, i, d_loss, g_loss, fidelity, m):
+    path_name = "./experiment/3bit/" + str(i) + "/" + noise_type + "/"
+
+    file_name = path_name + "d_loss.npy"
+    a = np.array(d_loss)
+    np.save(file_name, a)
+    # a = np.load(file_name)
+    # graphTable = a.tolist()
+    file_name = path_name + "g_loss.npy"
+    a = np.array(g_loss)
+    np.save(file_name, a)
+    file_name = path_name + "fidelity.npy"
+    a = np.array(fidelity)
+    np.save(file_name, a)
+
+    x = np.arange(0, m, 1)
+    plt.figure()
+    plt.xlabel("iteration")
+    plt.ylabel("d_loss")
+    plt.plot(x, d_loss, 'd')
+    file_name = path_name + "d_loss.png"
+    plt.savefig(file_name)
+    plt.figure()
+    plt.xlabel("iteration")
+    plt.ylabel("g_loss")
+    plt.plot(x, g_loss, 'd')
+    file_name = path_name + "g_loss.png"
+    plt.savefig(file_name)
+    plt.figure()
+    plt.xlabel("iteration")
+    plt.ylabel("fidelity")
+    plt.plot(x, fidelity, 'd')
+    file_name = path_name + "fidelity.png"
+    plt.savefig(file_name)
+
+
+def save_cir(type, i, cir):
+    file_name = "./experiment/3bit/" + str(i) + "/" + type + "/cir"
+    c = pickle.dumps(cir)
+    with open(file_name, "wb") as f:
+        f.write(c)
+    # f = open(file_name, "rb")
+    # obj = pickle.load(f)
+    # f.close()
+
+
+
+for i in range(1, 11):
+    cir = QC.QCircuitSimulator(2, 2, 2, 3, "Without_Noise")# noise_name = Bit_Flip,Phase_Flip, Bit_Phase_Flip, Depolarizing_Channel
+    save_cir("init", i, cir)
+
+    c = copy.deepcopy(cir)
+    fidelity, g_loss, d_loss = iteration(c, 100)
+    save_cir("without_noise", i, c)
+    save_figure("without_noise", i, d_loss, g_loss, fidelity, 100)
+
+    c = copy.deepcopy(cir)
+    c.noise_type = "Bit_Flip"
+    fidelity, g_loss, d_loss = iteration(c, 100)
+    save_cir("bit_flip", i, c)
+    save_figure("bit_flip", i, d_loss, g_loss, fidelity, 100)
+
+    c = copy.deepcopy(cir)
+    c.noise_type = "Phase_Flip"
+    fidelity, g_loss, d_loss = iteration(c, 100)
+    save_cir("phase_flip", i, c)
+    save_figure("phase_flip", i, d_loss, g_loss, fidelity, 100)
+
+    c = copy.deepcopy(cir)
+    c.noise_type = "Bit_Phase_Flip"
+    fidelity, g_loss, d_loss = iteration(c, 100)
+    save_cir("bit_phase_flip", i, c)
+    save_figure("bit_phase_flip", i, d_loss, g_loss, fidelity, 100)
+    
+    c = copy.deepcopy(cir)
+    c.noise_type = "Depolarizing_Channel"
+    fidelity, g_loss, d_loss = iteration(c, 100)
+    save_cir("depolarizing_channel", i, c)
+    save_figure("depolarizing_channel", i, d_loss, g_loss, fidelity, 100)
+
